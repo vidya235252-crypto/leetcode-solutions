bf7f71ef58e74e41ef57ae9be97e9411a65a4b7b
vidya235252-crypto <vidya235252@gmail.com>
2026-07-14 11:44:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (34.61%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int j;
+        for(int i=0;i<matrix.length;i++){
+            for(j=i+1;j<matrix.length;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            int left=0,right=matrix.length-1;
+            while(left<right){
+                int temp=matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
