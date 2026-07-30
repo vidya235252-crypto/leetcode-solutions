04db49d6d274e58170ffd939c0441233d5f7e1a3
vidya235252-crypto <vidya235252@gmail.com>
2026-07-30 12:22:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (43.95%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int left=0,right=nums.length-1;
+            int mid=0;
+        while(mid<=right){
+            if(nums[mid]==0){
+                int temp=nums[left];
+                nums[left]=nums[mid];
+                nums[mid]=temp;
+                left++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                int temp=nums[mid];
+                nums[mid]=nums[right];
+                nums[right]=temp;
+                right--;
+            }
+        }
+    }
+}
