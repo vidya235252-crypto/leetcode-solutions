a3b32cb91365b1ec960d20e06e0cc3e404e735ef
vidya235252-crypto <vidya235252@gmail.com>
2026-08-13 19:03:41 +0530

    Time: 0 ms (100.00%) | Memory: 80.2 MB (78.48%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int left=0,right=arr.length-2,res=-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(arr[mid]<arr[mid+1])
+            left=mid+1;
+            else{
+            res=mid;
+            right=mid-1;
+            }
+        }
+        return res;
+    }
+}
